3ae0f65566930818d9f408028a970a949e7374a7
Davinsicode <sureshroshan2@gmail.com>
2023-06-19 15:03:30 +0530

    WebdriverManager

--- src/test/java/executableFiles/BaseClass.java
+++ src/test/java/executableFiles/BaseClass.java
@@ -5,13 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseClass 
 {
 	public WebDriver driver;
 @BeforeClass
 public void openApplication()
 {
-	//WebDriverManager.chromedriver().setup();
+	WebDriverManager.chromedriver().setup();
 	driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://www.google.com");
